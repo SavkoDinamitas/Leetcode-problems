dbef8346d09d6a79a8e5ced2af3de25c85b07630
DimitrijeA <53818425+SavkoDinamitas@users.noreply.github.com>
2024-09-06 01:51:37 +0200

    interwiew 150 start

--- RobotSim.cpp
+++ RobotSim.cpp
@@ -6,39 +6,47 @@ using namespace std;
 https://leetcode.com/problems/walking-robot-simulation/submissions/1379290733/?envType=daily-question&envId=2024-09-04
 */
 
-int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        set<vector<int>> obs;
-        for(int i = 0; i < obstacles.size(); i++){
-            obs.insert(obstacles[i]);
+int robotSim(vector<int> &commands, vector<vector<int>> &obstacles)
+{
+    set<vector<int>> obs;
+    for (int i = 0; i < obstacles.size(); i++)
+    {
+        obs.insert(obstacles[i]);
+    }
+    int directionsX[4] = {0, 1, 0, -1};
+    int directionsY[4] = {1, 0, -1, 0};
+    int facing = 0;
+    int result = 0;
+    int sx = 0;
+    int sy = 0;
+    for (int i = 0; i < commands.size(); i++)
+    {
+        if (commands[i] == -1)
+        {
+            facing = (facing + 1) % 4;
         }
-        int directionsX [4] = {0, 1, 0, -1};
-        int directionsY [4] = {1, 0, -1, 0};
-        int facing = 0;
-        int result = 0;
-        int sx = 0;
-        int sy = 0;
-        for(int i = 0; i < commands.size(); i++){
-            if(commands[i] == -1){
-                facing = (facing + 1) % 4;
-            }
-            else if(commands[i] == -2){
-                facing = (facing + 3) % 4;
-            }
-            else{
-                for(int j = 0; j < commands[i]; j++){
-                    int nx = sx + directionsX[facing];
-                    int ny = sy + directionsY[facing];
-                    vector<int> pos = {nx, ny};
-                    if(obs.find(pos) == obs.end()){
-                        sx = nx;
-                        sy = ny;
-                        result = max(result, sx*sx + sy*sy);
-                    }
+        else if (commands[i] == -2)
+        {
+            facing = (facing + 3) % 4;
+        }
+        else
+        {
+            for (int j = 0; j < commands[i]; j++)
+            {
+                int nx = sx + directionsX[facing];
+                int ny = sy + directionsY[facing];
+                vector<int> pos = {nx, ny};
+                if (obs.find(pos) == obs.end())
+                {
+                    sx = nx;
+                    sy = ny;
+                    result = max(result, sx * sx + sy * sy);
                 }
             }
         }
-        return result;
     }
+    return result;
+}
 
 int main()
 {
---
+++ merge.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int l = 0;
+    int r = 0;
+    vector<int> c;
+    for(int i = 0; i < m; i++){
+        c.push_back(nums1[i]);
+    }
+
+    int i = 0;
+    while(l < m && r < n){
+        if(c[l] < nums2[r]){
+            nums1[i++] = c[l++];
+        }
+        else{
+            nums1[i++] = nums2[r++];
+        }
+    }
+
+    while(l < m){
+        nums1[i++] = c[l++];
+    }
+
+    while(r < n){
+        nums1[i++] = nums2[r++];
+    }
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
